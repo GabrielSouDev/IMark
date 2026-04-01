8761f7f1d83391495ed54a6180b297787838ef94
Gabriel <gabriel.calde21@gmail.com>
2026-04-01 13:40:03 -0300

    Alterando dominio de TimeEntry de mes/ano para dia especifico e removendo ViewModel para utilizar DTO

--- IMark.Shared/Models/DTO/TimeTrackings/TimeEntryDTO.cs
+++ IMark.Shared/Models/DTO/TimeTrackings/TimeEntryDTO.cs
@@ -5,6 +5,7 @@ namespace IMark.Shared.Models.DTO.TimeTrackings;
 public class TimeEntryDTO
 {
     public Guid Id { get; set; }
+    public DateOnly Date { get; set; }
     public List<TimeCheckDTO> Checks { get; set; } = new List<TimeCheckDTO>();
     public TimeEntryStatus Status { get; set; }
     public TimeSpan HoursWorked { get; set; }
