37f386bddfe81f0328638006ff1a17563574bfbb
Gabriel <gabriel.calde21@gmail.com>
2026-04-09 00:22:20 -0300

    Inclundo botão para adicionar horario em pagina de ajuste de ponto

--- IMark.Shared/Models/DTO/TimeTrackings/TimeCheckDTO.cs
+++ IMark.Shared/Models/DTO/TimeTrackings/TimeCheckDTO.cs
@@ -3,6 +3,11 @@ namespace IMark.Shared.Models.DTO.TimeTrackings;
 
 public class TimeCheckDTO
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime Timestamp { get; set; }
+    public DateTime TimestampLocal
+    {
+        get => DateTime.SpecifyKind(Timestamp, DateTimeKind.Utc).ToLocalTime();
+        set => Timestamp = DateTime.SpecifyKind(value, DateTimeKind.Local).ToUniversalTime();
+    }
 }
